cc169a6d4e659929aaad4d3315e7f7614d2081da
KaiHolden2026 <zih5094@psu.edu>
2024-04-30 22:32:36 -0400

    Initial commit

---
+++ routes/blogRoutes.js
@@ -0,0 +1,26 @@
+
+import express from 'express';
+import * as blogController from '../controllers/blogController.js'
+import * as userController from "../controllers/userController.js";
+import userRoutes from "./userRoutes.js";
+
+const router = express.Router();
+
+router.get('/', blogController.getAllBlogs);
+
+router.get('/:id', blogController.getBlogByID);
+
+
+router.post('/', blogController.createBlogPost);
+
+router.put('/like/:id', blogController.likeBlogComment);
+
+
+router.post('/:id/comment', blogController.addBlogComment);
+
+router.put('/:id/comment/like/:commentIndex', blogController.likeBlogComment);
+
+router.delete('/:id', blogController.deleteBlogPost);
+
+export default router;
+
---
+++ routes/userRoutes.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import * as userController from '../controllers/userController.js'
+
+const router = express.Router();
+
+router.get('/', userController.getAllUsers);
+
+
+router.get('/getUserByID', userController.getUserByID);
+
+
+router.post('/', userController.createUser)
+
+router.post('/login', userController.login)
+
+
+export default router;
